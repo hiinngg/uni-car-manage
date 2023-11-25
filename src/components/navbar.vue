fb3e686b114fd2c4ec5bace311b8a3bed1a6d2fa
lijinqing <498495737@qq.com>
2023-11-25 16:56:21 +0800

    init

---
+++ src/components/navbar.vue
@@ -0,0 +1,41 @@
+﻿<template>
+  <view class="navbar__container">
+        <view class="status_bar">
+            <!-- 这里是状态栏 -->
+        </view>
+        <view class="navbar__text">{{props.title}}</view>
+    </view>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    title:{
+        type:String
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+    .status_bar {
+        height: var(--status-bar-height);
+        width: 100%;
+    }
+
+    .navbar{
+        &__container{
+            height: 128rpx;
+            background-image: url('../static/navbar.png');
+        }
+
+        &__text{
+            text-align: center;
+            font-size: 36rpx;
+            font-family: PingFang SC;
+            font-weight: 400;
+            color: #333333;
+        }
+    }    
+
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+<navbar title="车场管理"></navbar>
+</template>
+
+<script setup lang="ts">
+import navbar from '@/components/navbar.vue'
+
+</script>
+
+<style>
+
+</style>
